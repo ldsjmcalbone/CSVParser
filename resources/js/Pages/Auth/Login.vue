a4175efffeca502f7760e0cd4d4560b42dd5ef9d
lds.jmcalbone <johnmichaelnoviciocalbone@gmail.com>
2022-11-07 11:23:35 +0800

    input error on login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,12 +1,16 @@
+
 <script >
 
 import { useForm } from '@inertiajs/inertia-vue3';
-
+import InputError from '@/Components/InputError.vue';
 export default({
     name: 'Login',
     props: {
         canResetPassword: Boolean,
         status: String,
+    },
+    components:{
+      InputError,
     },
     data(){
         return {
@@ -39,8 +43,9 @@ export default({
           
         </div>
         <div class="px-10 pt-4 pb-8 bg-white rounded-tr-4xl">
+          <InputError class="mt-2" :message="form.errors.email" />
           <h1 class="text-2xl font-semibold text-gray-900">HELLO! PLEASE SIGN IN!</h1>
-          <form class="mt-12" action="" method="POST">
+          
             <div class="relative">
               <input v-model="form.email" id="email" name="email" type="text" class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600" placeholder="john@doe.com" />
               <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email address</label>
@@ -56,7 +61,7 @@ export default({
             </div>
 
     
-          </form>
+         
           <a href="#" class="mt-4 block text-sm text-center font-medium text-green-600 hover:underline focus:outline-none focus:ring-2 focus:ring-rose-500"> Forgot your password? </a>
           
         </div>
